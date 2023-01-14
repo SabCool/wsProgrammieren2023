f725cb4c9baa388315f74b1d8d6a399e70e2e8cb
SabCool <sabine.classnitz@tumo.center>
2023-01-14 11:08:47 +0100

    expressBeispiel fertiggestellt

--- session3/index.js
+++ session3/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static("../gol"));
+
 app.get('/', function(req, res){
     res.send("<h2>Meine tolle Überschrift</h2><p>hier mein Text</p>");
 });
@@ -9,7 +11,19 @@ app.get("/name/:name", function(req, res){
     let name = req.params.name;
     res.send("<h1>Hello " + name +"</h1>");
  });
- 
+
+app.get('/gol', function(req, res){
+    res.redirect('index.html');
+});
+
+app.get('/google/:search', function(req, res){
+    let searchTerm = req.params.search;
+    res.redirect("https://www.google.com/search?q="+searchTerm);
+});
+
+app.get("/*", function(req, res) {
+    res.status(404).send("Uups geht nicht....");
+});
 
 app.listen(3000, function(){
     console.log("Mein Server läuft auf Port 3000");
